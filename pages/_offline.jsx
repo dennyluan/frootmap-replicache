7fdb91a9bacf36e188a85d684b65607fb28b887c
Denny Luan <dennyluan@gmail.com>
2021-07-15 02:48:27 -1000

    Cleanup

--- pages/_offline.jsx
+++ pages/_offline.jsx
@@ -2,9 +2,6 @@ import React from "react";
 
 export default () => (
   <>
-    <Head>
-      <title>next-pwa example</title>
-    </Head>
     <h1>This is offline fallback page</h1>
     <h2>When offline, any page route will fallback to this page</h2>
   </>
